286cf9ec5796165110a12e4ccff25ecd7908f9ba
Quinton Goos <qgoos@hotmail.com>
2024-02-19 00:07:31 -0500

    move helloworld package

---
+++ helloworld/hello.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	german = "German"
+	french = "French"
+
+	englishHelloPrefix = "Hello, "
+	germanHelloPrefix  = "Hallo, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+// Accepts: string, string default "Hello", default "English"
+// Returns: string;
+// Greets the provided string, defaulting to standard "Hello, World" when not provided
+func Hello(name string, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+// Accepts: language string; default "English"
+// Returns: prefix string
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case german:
+		prefix = germanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
