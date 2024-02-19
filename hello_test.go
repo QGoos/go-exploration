621a5bb4344770ee6ea4400d7bb49ccaaf0a8ce5
Quinton Goos <qgoos@hotmail.com>
2024-02-18 23:27:56 -0500

    refactor tests

--- hello_test.go
+++ hello_test.go
@@ -5,9 +5,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when empty string supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 
 const englishHelloPrefix = "Hello, "
 
+// Accepts string default "Hello";
+// Returns string;
+// Greets the provided string, defaulting to standard "Hello, World" when not provided
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
