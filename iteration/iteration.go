1cb18b307bea631e67d1b0ff465f5ce8b85d35bf
Quinton Goos <qgoos@hotmail.com>
2024-02-21 05:26:09 -0500

    create CRUD API for a dictionary

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,5 +1,74 @@
 package iteration
 
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("could not update definition because the word does not exist")
+)
+
+type Dictionary map[string]string
+type DictionaryErr string
+
+// cool, error interface for Dictionary
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Accepts: word string
+// Returns: definition string, err error
+// check for word defined in the dicitonary
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+// Accepts: word, definition string
+// Returns: err error
+// Add a word and definition pair to the dictionary
+func (d Dictionary) Add(word string, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// Accepts: word, definition string
+// Returns: err error
+// Update definition paired to word in to the dictionary
+func (d Dictionary) Update(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// Accepts: word to be deleted
+// Delete word definition pair in dicitonary
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
 // Accepts: character string
 // Accepts: count integer
 // Returns: string
@@ -37,4 +106,12 @@ func SumSlices(nums ...[]int) []int {
 	}
 
 	return sums
+}
+
+// Accepts: dictionary map[string]string
+// Accepts: word string
+// Returns: definition string
+// check for word defined in the dicitonary
+func Search(dictionary map[string]string, target string) string {
+	return dictionary[target]
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -26,6 +26,86 @@ func TestSumSlices(t *testing.T) {
 	}
 }
 
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is jsut a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is jsut a test"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unkown")
+		want := ErrNotFound
+
+		assertError(t, err, want)
+	})
+
+}
+
+func TestAdd(t *testing.T) {
+
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		dictionary.Add("test", "this is just a test")
+
+		want := "this is just a test"
+		got, err := dictionary.Search("test")
+
+		assertError(t, err, nil)
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		got, _ := dictionary.Search("test")
+
+		assertError(t, err, ErrWordExists)
+		assertCorrectMessage(t, got, definition)
+	})
+
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("word exists", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		dictionary.Update(word, newDefinition)
+		got, err := dictionary.Search("test")
+
+		assertError(t, err, nil)
+		assertCorrectMessage(t, got, newDefinition)
+	})
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("word exists", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{word: "test definition"}
+
+		dictionary.Delete(word)
+
+		_, err := dictionary.Search(word)
+		assertError(t, err, ErrNotFound)
+	})
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
@@ -36,5 +116,12 @@ func assertCorrectMessage(t testing.TB, got any, want any) {
 	t.Helper()
 	if got != want {
 		t.Errorf("expected '%v' but got '%v'", want, got)
+	}
+}
+
+func assertError(t testing.TB, err error, want error) {
+	t.Helper()
+	if err != want {
+		t.Errorf("got error %q want %q", err, want)
 	}
 }
